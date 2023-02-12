038c6fb69e7d3dd645ea1bd068bb6099f461c0c3
Jimmy Yeh <chienfeng0719@gmail.com>
2023-02-12 08:31:49 +0800

    Add vuter config

---
+++ vuter.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    {
+      root: './hi-manta-web', // root of your vue project (should contain package.json)
+      package: './hi-manta-web/package.json', // Relative to root property, don't change this.
+      tsconfig: './hi-manta-web/tsconfig.json',  // Relative to root property, don't change this.
+    }
+  ]
+}
