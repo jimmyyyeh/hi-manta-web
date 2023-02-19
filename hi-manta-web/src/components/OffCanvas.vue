b333a6023d5cd67f53871b0b21debb6c0a206aef
Jimmy Yeh <chienfeng0719@gmail.com>
2023-02-19 17:14:05 +0800

    Fix OffCanvas

--- hi-manta-web/src/components/OffCanvas.vue
+++ hi-manta-web/src/components/OffCanvas.vue
@@ -6,6 +6,10 @@
     <div class="offcanvas-body">
       <ul>
         <li @click="redirectPage('home')" data-bs-dismiss="offcanvas">首頁</li>
+        <li @click="redirectPage('medalLog')" data-bs-dismiss="offcanvas">勳章紀錄</li>
+        <li @click="redirectPage('medal')" data-bs-dismiss="offcanvas">勳章列表</li>
+        <li @click="redirectPage('prize')" data-bs-dismiss="offcanvas">獎勵列表</li>
+        <li @click="redirectPage('auth')" data-bs-dismiss="offcanvas">{{ $parent.isSignIn ? '登出': '登入' }}</li>
       </ul>
     </div>
   </div>
@@ -17,6 +21,10 @@ export default {
     return {
       pathMap: {
         home: '/',
+        auth: '/auth',
+        medal: '/medal',
+        prize: '/prize',
+        medalLog: '/medal-log',
       },
     };
   },
